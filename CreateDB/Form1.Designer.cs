e1f48f2eb1319256e5f3498fa18e9af4fbdb3283
RONAK M DHAMELIYA <64737207+dev-ronakdhameliya@users.noreply.github.com>
2024-03-04 12:01:23 +0530

    Primary upload

---
+++ CreateDB/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace CreateDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblMsg = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CmbServers = new System.Windows.Forms.ComboBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.BtnCreateDb = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtFilePath = new System.Windows.Forms.TextBox();
+            this.BtnBrows = new System.Windows.Forms.Button();
+            this.BtnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LblMsg
+            // 
+            this.LblMsg.Location = new System.Drawing.Point(145, 124);
+            this.LblMsg.Name = "LblMsg";
+            this.LblMsg.Size = new System.Drawing.Size(300, 20);
+            this.LblMsg.TabIndex = 15;
+            this.LblMsg.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.LblMsg.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(60, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 26);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Server";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // CmbServers
+            // 
+            this.CmbServers.FormattingEnabled = true;
+            this.CmbServers.Location = new System.Drawing.Point(145, 24);
+            this.CmbServers.Name = "CmbServers";
+            this.CmbServers.Size = new System.Drawing.Size(330, 26);
+            this.CmbServers.TabIndex = 13;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(145, 144);
+            this.progressBar1.MarqueeAnimationSpeed = 20;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(300, 10);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 12;
+            this.progressBar1.Visible = false;
+            // 
+            // BtnCreateDb
+            // 
+            this.BtnCreateDb.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnCreateDb.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCreateDb.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BtnCreateDb.Location = new System.Drawing.Point(145, 90);
+            this.BtnCreateDb.Name = "BtnCreateDb";
+            this.BtnCreateDb.Size = new System.Drawing.Size(130, 30);
+            this.BtnCreateDb.TabIndex = 11;
+            this.BtnCreateDb.Text = "Create Database";
+            this.BtnCreateDb.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(60, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 26);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "File path";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // TxtFilePath
+            // 
+            this.TxtFilePath.Location = new System.Drawing.Point(145, 57);
+            this.TxtFilePath.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtFilePath.Name = "TxtFilePath";
+            this.TxtFilePath.Size = new System.Drawing.Size(300, 26);
+            this.TxtFilePath.TabIndex = 9;
+            // 
+            // BtnBrows
+            // 
+            this.BtnBrows.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.BtnBrows.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnBrows.Location = new System.Drawing.Point(445, 57);
+            this.BtnBrows.Name = "BtnBrows";
+            this.BtnBrows.Size = new System.Drawing.Size(30, 26);
+            this.BtnBrows.TabIndex = 16;
+            this.BtnBrows.Text = "...";
+            this.BtnBrows.UseVisualStyleBackColor = true;
+            // 
+            // BtnClose
+            // 
+            this.BtnClose.BackColor = System.Drawing.Color.Red;
+            this.BtnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnClose.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnClose.ForeColor = System.Drawing.Color.White;
+            this.BtnClose.Location = new System.Drawing.Point(281, 90);
+            this.BtnClose.Name = "BtnClose";
+            this.BtnClose.Size = new System.Drawing.Size(70, 30);
+            this.BtnClose.TabIndex = 17;
+            this.BtnClose.Text = "Close";
+            this.BtnClose.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(534, 161);
+            this.Controls.Add(this.BtnClose);
+            this.Controls.Add(this.BtnBrows);
+            this.Controls.Add(this.LblMsg);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CmbServers);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.BtnCreateDb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtFilePath);
+            this.Font = new System.Drawing.Font("Calibri", 11.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Create DB";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblMsg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CmbServers;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button BtnCreateDb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtFilePath;
+        private System.Windows.Forms.Button BtnBrows;
+        private System.Windows.Forms.Button BtnClose;
+    }
+}
+
---
+++ CreateDB/Form1.cs
@@ -0,0 +1,381 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.IO;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CreateDB
+{
+    public partial class Form1 : Form
+    {
+        private const string ConnectionString = "Server={0};Database={1};integrated security=true;Trusted_Connection=True;MultipleActiveResultSets=true;";
+
+        //Integrated Security=SSPI;TrustServerCertificate=True;
+
+        private SqlConnection connection = null;
+        private readonly string DatabaseName = ConfigurationManager.AppSettings["DatabaseName"];
+        private readonly string SchemaFilePath = AppDomain.CurrentDomain.BaseDirectory + ConfigurationManager.AppSettings["Schema"];
+        private readonly string DbObjectFilePath = AppDomain.CurrentDomain.BaseDirectory + ConfigurationManager.AppSettings["DbObjects"];
+        private readonly string MasterDataFilePath = AppDomain.CurrentDomain.BaseDirectory + ConfigurationManager.AppSettings["MasterData"];
+        private readonly BackgroundWorker bgw = new BackgroundWorker { WorkerReportsProgress = true, WorkerSupportsCancellation = true };
+
+        public Form1()
+        {
+            InitializeComponent();
+            BtnCreateDb.Click += BtnCreateDb_Click;
+            BtnClose.Click += (s, e) => { Application.ExitThread(); };
+            BtnBrows.Click += BtnBrows_Click;
+            bgw.DoWork += Bgw_DoWork;
+            bgw.RunWorkerCompleted += Bgw_RunWorkerCompleted;
+        }
+
+        private void BtnBrows_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            DialogResult result = folderBrowser.ShowDialog();
+
+            if (result == DialogResult.OK)
+            { TxtFilePath.Text = folderBrowser.SelectedPath; }
+        }
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            LblMsg.Text = "Please wait while collecting databases...";
+            DisableControls();
+            bgw.RunWorkerAsync();
+        }
+
+        private void Bgw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            List<string> Parameters = (List<string>)e.Result;
+            CmbServers.DataSource = Parameters;
+            progressBar1.Visible = false;
+            EnableControls();
+        }
+
+        private void Bgw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            DataTable table = SqlDataSourceEnumerator.Instance.GetDataSources();
+            List<string> Parameters = new List<string>();
+
+            foreach (DataRow row in table.Rows)
+            {
+                if (!string.IsNullOrEmpty(row["InstanceName"].ToString()))
+                { Parameters.Add(row["ServerName"].ToString() + "\\" + row["InstanceName"].ToString()); }
+            }
+
+            e.Result = Parameters;
+        }
+
+        private void BtnCreateDb_Click(object sender, EventArgs e)
+        {
+            DisableControls();
+            if (string.IsNullOrEmpty(DatabaseName))
+            {
+                MessageBox.Show("Database name not defined.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(TxtFilePath.Text))
+            {
+                MessageBox.Show("Please enter path to create database file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                return;
+            }
+            else if (!Directory.Exists(TxtFilePath.Text))
+            {
+                MessageBox.Show("Given path is not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                return;
+            }
+
+            ShowMessage("Creating Database...");
+            connection = new SqlConnection(string.Format(ConnectionString, CmbServers.Text, "master"));
+
+            bool IsDbCreated = false;
+            Invoke(new MethodInvoker(delegate { IsDbCreated = CreateDatabase(connection); }));
+
+            if (IsDbCreated && File.Exists(SchemaFilePath))
+            {
+                if (!File.Exists(SchemaFilePath))
+                {
+                    MessageBox.Show("Schema file not found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                    return;
+                }
+
+                ShowMessage("Creating Schemas...");
+                connection = new SqlConnection(string.Format(ConnectionString, CmbServers.Text, DatabaseName));
+
+                bool IsSchemaCreated = false;
+                Invoke(new MethodInvoker(delegate { IsSchemaCreated = CreateSchema(connection); }));
+
+                if (IsSchemaCreated)
+                {
+                    if (!File.Exists(DbObjectFilePath))
+                    {
+                        MessageBox.Show("Database object file not found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                        return;
+                    }
+
+                    ShowMessage("Creating database objects...");
+                    this.connection = new SqlConnection(string.Format(ConnectionString, CmbServers.Text, DatabaseName));
+
+                    bool IsDbObjectCreated = false;
+                    Invoke(new MethodInvoker(delegate { IsDbObjectCreated = CreateDbObjects(connection); }));
+
+                    if (IsDbObjectCreated)
+                    {
+                        if (!File.Exists(MasterDataFilePath))
+                        {
+                            MessageBox.Show("Master data file not found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                            return;
+                        }
+
+                        ShowMessage("Inserting master records...");
+                        bool IsMasterDataInserted = false;
+                        Invoke(new MethodInvoker(delegate { IsMasterDataInserted = InsertMasterData(connection); }));
+
+                        if (IsMasterDataInserted)
+                        {
+                            ShowMessage("Database created successfully...");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                ShowMessage("Database not created...");
+            }
+
+            EnableControls();
+        }
+
+        public bool CreateDatabase(SqlConnection connection)
+        {
+            string appPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            GrantAccess(appPath);
+            bool IsExits = CheckDatabaseExists(connection, DatabaseName);
+
+            if (!IsExits)
+            {
+                string CreateDatabase = $" CREATE DATABASE ApexAMCS ON PRIMARY " +
+                                        $"(NAME = '{DatabaseName}', " +
+                                        $"FILENAME = '{TxtFilePath.Text.Trim()}\\{DatabaseName}.mdf', " +
+                                        $"SIZE = 10, " +
+                                        $"MAXSIZE = " +
+                                        $"UNLIMITED, " +
+                                        $"FILEGROWTH = 5) " +
+                                        $"LOG ON " +
+                                        $"(NAME = '{DatabaseName}_Log', " +
+                                        $"FILENAME = '{TxtFilePath.Text.Trim()}\\{DatabaseName}_Log.ldf', " +
+                                        $"SIZE = 10, " +
+                                        $"MAXSIZE = UNLIMITED, " +
+                                        $"FILEGROWTH = 5) ";
+
+                SqlCommand command = new SqlCommand(CreateDatabase, connection);
+
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return false;
+                }
+                finally
+                {
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        connection.Close();
+                    }
+                }
+                return true;
+            }
+            else
+            { return true; }
+        }
+
+        public bool CreateSchema(SqlConnection connection)
+        {
+            bool IsExits = CheckDatabaseExists(connection, DatabaseName); //Check database exists in sql server.
+
+            if (IsExits)
+            {
+                string CreateSchema = File.ReadAllText(SchemaFilePath);
+
+                SqlCommand command = new SqlCommand(CreateSchema.Replace("GO", string.Empty), connection);
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    ex.ToString();
+                }
+                finally
+                {
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        connection.Close();
+                    }
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool CreateDbObjects(SqlConnection connection)
+        {
+            bool IsExits = CheckDatabaseExists(connection, DatabaseName); //Check database exists in sql server.
+
+            if (IsExits)
+            {
+                string CreateSchema = File.ReadAllText(DbObjectFilePath);
+                MatchCollection objects = Regex.Matches(CreateSchema, "CREATE PROCEDURE.*?(?=SET ANSI_NULLS ON)", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
+                foreach (Match match in objects)
+                {
+                    SqlCommand command = new SqlCommand(Regex.Match(match.Value.Trim(), ".*?(?=GO)", RegexOptions.IgnoreCase | RegexOptions.Singleline).Value.Trim(), connection);
+
+                    try
+                    {
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        ex.ToString();
+                    }
+                    finally
+                    {
+                        if (connection.State == ConnectionState.Open)
+                        {
+                            connection.Close();
+                        }
+                    }
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool InsertMasterData(SqlConnection connection)
+        {
+            bool IsExits = CheckDatabaseExists(connection, DatabaseName); //Check database exists in sql server.
+
+            if (IsExits)
+            {
+                string CreateDatabase = File.ReadAllText(MasterDataFilePath);
+                SqlCommand command = new SqlCommand(CreateDatabase.Replace("GO", string.Empty), connection);
+
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return false;
+                }
+                finally
+                {
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        connection.Close();
+                    }
+                }
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool GrantAccess(string fullPath)
+        {
+            try
+            {
+                DirectoryInfo info = new DirectoryInfo(fullPath);
+                WindowsIdentity self = System.Security.Principal.WindowsIdentity.GetCurrent();
+                DirectorySecurity ds = info.GetAccessControl();
+                ds.AddAccessRule(new FileSystemAccessRule(self.Name, FileSystemRights.FullControl, InheritanceFlags.ObjectInherit | InheritanceFlags.ContainerInherit, PropagationFlags.None, AccessControlType.Allow));
+                info.SetAccessControl(ds);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool CheckDatabaseExists(SqlConnection tmpConn, string databaseName)
+        {
+            string sqlCreateDBQuery;
+            bool result = false;
+
+            try
+            {
+                sqlCreateDBQuery = string.Format("SELECT database_id FROM sys.databases WHERE Name = '{0}'", databaseName);
+                using (SqlCommand sqlCmd = new SqlCommand(sqlCreateDBQuery, tmpConn))
+                {
+                    tmpConn.Open();
+                    object resultObj = sqlCmd.ExecuteScalar();
+                    int databaseID = 0;
+
+                    if (resultObj != null)
+                    { int.TryParse(resultObj.ToString(), out databaseID); }
+
+                    tmpConn.Close();
+                    result = (databaseID > 0);
+                }
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+            return result;
+        }
+
+        private void EnableControls()
+        {
+            LblMsg.Visible = false;
+            progressBar1.Visible = false;
+            CmbServers.Enabled = true;
+            TxtFilePath.Enabled = true;
+            BtnCreateDb.Enabled = true;
+            CmbServers.Focus();
+        }
+
+        private void DisableControls()
+        {
+            LblMsg.Visible = true;
+            progressBar1.Visible = true;
+            CmbServers.Enabled = false;
+            TxtFilePath.Enabled = false;
+            BtnCreateDb.Enabled = false;
+        }
+
+        private void ShowMessage(string Message)
+        {
+            Invoke(new MethodInvoker(delegate
+            {
+                Thread.Sleep(2000);
+                LblMsg.Text = Message;
+            }));
+        }
+    }
+}
